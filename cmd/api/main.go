17f836703a972474e96cb886534c72aa62d03451
lyubomira <lyubomira@gtmhub.com>
2023-02-12 22:31:58 +0200

    connecting to Postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,23 +12,38 @@ import (
 const port = 8080
 
 type application struct {
+	DSN string
 	Domain string
+	DB repository.DatabaseRepo
 }
+
+// docker-compose up -d
+// go run ./cmd/api 
+// docker-compose down
+
 
 func main()  {
 	// set application config
 	var app application
 
 	// read from command line 
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// connect to the database
+	conn, err :=app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 
 	app.Domain = "example.com"
 
 	log.Println("Starting  application on port", port)
 
 	// start a web server 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module backend
 
 go 1.14
 
-require github.com/go-chi/chi/v5 v5.0.8
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/jackc/pgconn v1.13.0
+	github.com/jackc/pgx/v4 v4.17.2
+)
