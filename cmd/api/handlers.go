b11e03cfc7982728110dd2da21d1ad84b78684a8
lyubomira <lyubomira@gtmhub.com>
2023-02-13 21:06:57 +0200

    added JSON handler util functions

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request)  {
-	// fmt.Fprintf(w, "Hello world from %s", app.Domain)
-
 	var payload = struct {
 		Status string `json:"status"`
 		Message string `json:"message"`
@@ -20,14 +16,7 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request)  {
 		Version: "6.6.6",
 	}
 
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, payload)
 }
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request)  {
@@ -35,16 +24,9 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request)  {
 	movies, err := app.DB.AllMovies()
 
 	if err != nil {
-		fmt.Println(err)
+		app.errorJSON(w, err)
 		return
 	}
 
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.writeJSON(w, http.StatusOK, movies)
 }
